0efb454ff238b166464d50e1ed47c8305b91df7f
Joyce-Zo <109672607+Joyce-Zo@users.noreply.github.com>
2022-12-22 23:51:01 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,27 @@
+#include "DList.h"
+
+void testList1()
+{
+	LTNode* plist = ListInit();
+
+	ListPushBack(plist, 1);
+	ListPushBack(plist, 2);
+	ListPushBack(plist, 3);
+	ListPushBack(plist, 4);
+	ListPushBack(plist, 5);
+	ListPrint(plist);
+	
+	ListPopBack(plist);
+	ListPopBack(plist);
+	ListPopBack(plist);
+	ListPopBack(plist);
+	ListPopBack(plist);
+	ListPopBack(plist);
+	ListPrint(plist);
+}
+int main()
+{
+	testList1();
+
+	return;
+}
